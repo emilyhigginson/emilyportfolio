27a57c81b308bb6e6898711221df54eaabc112c9
Emily <emilyhigginson14@gmail.com>
2022-04-10 18:00:20 -0400

    edits

--- src/About.jsx
+++ src/About.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-
+import Navbar from './Navbar'
 function About() {
   return (
+      
     <div id="about">
+ 
         <div>
         <h1>Hi, I'm Emily! </h1>
-        <div>I am a Software Developer experienced in JavaScript and Ruby on Rails based programming. As a former Spanish teacher, I possess strong skills in critical thinking, collaboration and organization that will help me serve as an excellent entry level software developer. I am seeking to further improve my skills with a company that encourages constant growth and exposure to new knowledge. I am self-motivated, determined and a source of positive energy.</div>
+        <div>I am a Software Developer experienced in JavaScript and Ruby on Rails based programming. As a former Spanish teacher, I possess strong skills in leadership, empathy and organization that will help me serve as an excellent entry level software developer. I am seeking to further improve my skills with a company that encourages constant growth and exposure to new knowledge. I am self-motivated, determined and a source of positive energy.</div>
         <div>When I'm not coding, I enjoy reading, staying active, good music, & exploring new cities with my friends </div>
+        <p> Programming Languages & Frameworks: Javascript (ES6), React, Ruby on Rails <br></br> Skills: Git, CSS, HTML, ActiveRecord, VS Code, SQL, PostgreSQL
+        </p>
         <a id='proflink' href='https://www.linkedin.com/in/eehigginson/'> Linkedin</a> 
-        <a id='proflink' href='https://github.com/emilyhigginson'> Github</a>
+        <a id='proflink' href='https://github.com/emilyhigginson'>Github</a>
         </div>
 <div> 
     <img src='./emily.png'></img>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import {Link } from 'react-scroll'
-import About from './About'
 function Navbar() {
   return (
     <div className='navbar'>
